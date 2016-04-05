42a07eacdae6bf0d864e08b892390f88750c52a2
Thorsten Heimes <thorstentheimes@gmail.com>
2016-04-05 16:30:37 +0200

    Added Annotation to Functional Interfaces

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -1,41 +1,43 @@
 package lambdasinaction.chap3;
 
 import java.io.*;
+
 public class ExecuteAround {
 
-	public static void main(String ...args) throws IOException{
+  public static void main(String... args) throws IOException {
 
-        // method we want to refactor to make more flexible
-        String result = processFileLimited();
-        System.out.println(result);
+    // method we want to refactor to make more flexible
+    String result = processFileLimited();
+    System.out.println(result);
 
-        System.out.println("---");
+    System.out.println("---");
 
-		String oneLine = processFile((BufferedReader b) -> b.readLine());
-		System.out.println(oneLine);
+    String oneLine = processFile((BufferedReader b) -> b.readLine());
+    System.out.println(oneLine);
 
-		String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
-		System.out.println(twoLines);
+    String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
+    System.out.println(twoLines);
 
-	}
+  }
 
-    public static String processFileLimited() throws IOException {
-        try (BufferedReader br =
-                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
-            return br.readLine();
-        }
+  public static String processFileLimited() throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(
+        "/Users/thorsten/development/java8ia/Java8InAction/src/main/resources/lambdasinaction/chap3/data.txt"))) {
+      return br.readLine();
+    }
+  }
+
+  public static String processFile(BufferedReaderProcessor p) throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(
+        "/Users/thorsten/development/java8ia/Java8InAction/src/main/resources/lambdasinaction/chap3/data.txt"))) {
+      return p.process(br);
     }
 
+  }
 
-	public static String processFile(BufferedReaderProcessor p) throws IOException {
-		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
-			return p.process(br);
-		}
+  @FunctionalInterface
+  public interface BufferedReaderProcessor {
+    public String process(BufferedReader b) throws IOException;
 
-	}
-
-	public interface BufferedReaderProcessor{
-		public String process(BufferedReader b) throws IOException;
-
-	}
+  }
 }
