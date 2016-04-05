9fd75fd4a49abe9ae4541e8495e2cbaa171f4c71
Thorsten Heimes <thorstentheimes@gmail.com>
2016-04-05 16:40:33 +0200

    Example for filter with Predicate

---
+++ src/main/java/lambdasinaction/chap3/PredicateUsage.java
@@ -0,0 +1,31 @@
+package lambdasinaction.chap3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateUsage {
+
+  public static void main(String[] args) {
+    List<String> listOfString = Arrays.asList("hello", "flexible", "", "lambda", "", "world" ); 
+
+    Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+
+    List<String> nonEmpty = filter(listOfString, nonEmptyStringPredicate);
+    
+    for(String s : nonEmpty) {
+      System.out.println(s);
+    }
+
+  }
+  public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+    List<T> result = new ArrayList<>();
+    for (T s : list) {
+      if(p.test(s)){
+        result.add(s);
+      }
+    }
+    return result;
+  }
+}
