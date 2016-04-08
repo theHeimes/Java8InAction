27ac98ab1fc3990e07a076ab0d19e525e0cb9dba
Thorsten Heimes <thorstentheimes@gmail.com>
2016-04-08 21:38:33 +0200

    Elaborating DishTypes with stream preview.

--- src/main/java/lambdasinaction/chap4/DishTypes.java
+++ src/main/java/lambdasinaction/chap4/DishTypes.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import static java.util.stream.Collectors.groupingBy;
 
 import lambdasinaction.chap4.Dish.Type;
 
@@ -13,7 +14,6 @@ public class DishTypes {
   public static void main(String[] args) {
     
     Map<Dish.Type, List<Dish>> dishesByType = new HashMap<>();
-    
     // initialize dishesByType
     for (Type t : Type.values()) {
       dishesByType.put(t, new ArrayList<Dish>());
@@ -24,10 +24,22 @@ public class DishTypes {
       dishesByType.put(d.getType(), l);
     }
     
+    System.out.println("Menu by types in Java 7");
     Set<Type> keys = dishesByType.keySet();
-    
     for (Type key : keys) {
       System.out.printf("%-6s=%s\n", key, dishesByType.get(key));
     }
+    
+    // And just for fun now in Java 8 style
+    System.out.println("\nMenu by types in Java 8");
+    
+    Map<Dish.Type, List<Dish>> dishesByType8 =
+        Dish.menu.parallelStream().collect(groupingBy(Dish::getType));
+    
+    keys = dishesByType8.keySet();
+    for (Type key : keys) {
+      System.out.printf("%-6s=%s\n", key, dishesByType8.get(key));
+    }
+     
   }
 }
