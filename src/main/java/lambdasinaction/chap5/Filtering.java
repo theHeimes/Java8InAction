a86a2c316dc4fa5fa68ff617319c0bdcbb2b5c6e
Thorsten Heimes <thorstentheimes@gmail.com>
2016-04-12 12:02:09 +0200

    Tinkering with map and flatMap

--- src/main/java/lambdasinaction/chap5/Filtering.java
+++ src/main/java/lambdasinaction/chap5/Filtering.java
@@ -17,15 +17,18 @@ public class Filtering{
                 .filter(Dish::isVegetarian)
                 .collect(toList());
 
+        System.out.printf("Vegetarian menu:\n");
         vegetarianMenu.forEach(System.out::println);
 
         // Filtering unique elements
         List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        System.out.printf("\nDistinct even numbers:\n");
+        System.out.println(numbers);
         numbers.stream()
                .filter(i -> i % 2 == 0)
                .distinct()
                .forEach(System.out::println);
-
+        
         // Truncating a stream
         List<Dish> dishesLimit3 =
             menu.stream()
@@ -33,6 +36,7 @@ public class Filtering{
                 .limit(3)
                 .collect(toList());
 
+        System.out.printf("\nLimit dishes:\n");
         dishesLimit3.forEach(System.out::println);
 
         // Skipping elements
@@ -42,6 +46,7 @@ public class Filtering{
                 .skip(2)
                 .collect(toList());
 
+        System.out.printf("\nSkip dishes:\n");
         dishesSkip2.forEach(System.out::println);
     }
 }
--- src/main/java/lambdasinaction/chap5/Mapping.java
+++ src/main/java/lambdasinaction/chap5/Mapping.java
@@ -20,15 +20,46 @@ public class Mapping{
         List<String> words = Arrays.asList("Hello", "World");
         List<Integer> wordLengths = words.stream()
                                          .map(String::length)
+                                         //.distinct()
                                          .collect(toList());
         System.out.println(wordLengths);
+        
+        List<Integer> dishNameLengths = menu.stream()
+            .map(Dish::getName)
+            .map(String::length)
+            .sorted()
+            .distinct()
+            .collect(toList());
+        System.out.printf("\nLengths of dish names: %s\n", dishNameLengths);
 
+        System.out.printf("\nShowing the need for flatMap\n");
+        // map
+        words.stream()
+                 .map(word -> word.split(""))
+                 .distinct()
+                 .map(c -> c.getClass())
+                 .forEach(System.out::println);
+        System.out.println("\nTinkering with map:");
+        words.stream()
+          .map(word -> word.split(""))
+          .flatMap(Arrays::stream)
+//          .map(c -> c.getClass())
+          .distinct()
+          .forEach(System.out::println);
+        System.out.printf("\nHere it comes\n");
         // flatMap
         words.stream()
                  .flatMap((String line) -> Arrays.stream(line.split("")))
                  .distinct()
                  .forEach(System.out::println);
+        
+        System.out.println("\nQuiz 5.2:");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        numbers.stream()
+          .map(i -> i * i)
+          .forEach(System.out::println);
 
+        System.out.println("\nFlat Map combining two lists (Quiz 5.3):");
         // flatMap
         List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
         List<Integer> numbers2 = Arrays.asList(6,7,8);
@@ -40,5 +71,16 @@ public class Mapping{
                                 .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
                                 .collect(toList());
         pairs.forEach(pair -> System.out.println("(" + pair[0] + ", " + pair[1] + ")"));
+        
+        System.out.println("\nFlat Map combining two lists (Quiz 5.3) - 2:");
+        numbers1 = Arrays.asList(1,2,3,4,5,6);
+        List<int[]> pairs2 =
+            numbers1.stream()
+                    .flatMap((Integer i) -> numbers2.stream()
+                                           .map((Integer j) -> new int[]{i, j})
+                     )
+                    .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
+                    .collect(toList());
+        pairs2.forEach(pair -> System.out.println("(" + pair[0] + ", " + pair[1] + ")"));
     }
 }
